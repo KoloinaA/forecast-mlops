0c463348c32c74f833f2632aeaaa0cd14b5c4522
Koloina Rajaonarivony <koloina.arilala@gmail.com>
2026-07-15 10:44:30 +0300

    Add split and train data

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from trainmodel.training.loader import load_training_data
+from trainmodel.training.split import temporal_split
+
 
 df = load_training_data()
 
-print(df.head())
 
-print(df.shape)
+X_train, X_test, y_train, y_test = temporal_split(df)
+
+
+print(X_train.shape)
+print(X_test.shape)
+
+
+print(y_train.shape)
+print(y_test.shape)
---
+++ trainmodel/training/split.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+
+def temporal_split(
+        df: pd.DataFrame,
+        target: str = "sales"
+):
+
+    # Toujours trier par date
+    df = df.sort_values("date")
+
+
+    # Nombre de dates uniques
+    n_dates = df["date"].nunique()
+
+
+    # Point de séparation
+    split_point = int(n_dates * 0.8)
+
+
+    dates = (
+        df["date"]
+        .drop_duplicates()
+        .sort_values()
+    )
+
+
+    train_dates = dates.iloc[:split_point]
+
+    test_dates = dates.iloc[split_point:]
+
+
+    train = df[
+        df["date"].isin(train_dates)
+    ]
+
+
+    test = df[
+        df["date"].isin(test_dates)
+    ]
+
+
+    # Séparation X / y
+
+    X_train = train.drop(
+        columns=[target, "date"]
+    )
+
+    y_train = train[target]
+
+
+    X_test = test.drop(
+        columns=[target, "date"]
+    )
+
+    y_test = test[target]
+
+
+    return (
+        X_train,
+        X_test,
+        y_train,
+        y_test
+    )
